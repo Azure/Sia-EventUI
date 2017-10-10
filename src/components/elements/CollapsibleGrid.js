83310d3f9fa0556725867e8d4ab0959a25d195db
Jackie Cheung <jache@microsoft.com>
2017-10-10 14:58:06 -0700

    fixing and updating collapse icons

--- src/components/elements/CollapsibleGrid.js
+++ src/components/elements/CollapsibleGrid.js
@@ -1,25 +1,27 @@
 import React from 'react'
 import { GridSet } from './Grid'
 import IconButtonStyled from '../elements/IconButtonStyled'
-import CodeIcon from 'material-ui/svg-icons/action/code'
+import ArrowDropDownIcon from 'material-ui/svg-icons/navigation/arrow-drop-down'
+import ArrowDropDownCircleIcon from 'material-ui/svg-icons/navigation/arrow-drop-down-circle'
 import * as expandSectionActions from '../../actions/expandSectionActions'
+
+const dispatchOnTouchTap = (collapseName, dispatch) => () => dispatch(expandSectionActions.toggleCollapse(collapseName))
 
 export const CollapsibleGridSet = (containerClass, rowClass, columnClass, children, collapseNames, collapseState, dispatch) => {
     let collapseKey = 0
-    let collapseStatus = collapseNames.map(name => collapseState[name])
+    const collapseStatus = collapseNames.map(name => collapseState[name])
     return GridSet (containerClass, rowClass, columnClass, children.map(child => {
-        let currentChild = collapseStatus[collapseKey] ? child.slice(0,1) : child
+        const isCollapsed = collapseStatus[collapseKey]
+        const currentChild = isCollapsed ? child.slice(0,1) : child
+        const currentName = collapseNames[collapseKey]
         currentChild[0][0].push(
             (key) =>
             <IconButtonStyled
                 tooltip="Collapse/expand section"
-                onTouchTap={() => {
-                    debugger
-                    dispatch(expandSectionActions.toggleCollapse(collapseNames[collapseKey]))}
-                }
+                onTouchTap={dispatchOnTouchTap(currentName, dispatch)}
                 key={key}
             >
-                <CodeIcon />
+                {isCollapsed ? <ArrowDropDownCircleIcon /> : <ArrowDropDownIcon />}
             </IconButtonStyled>
         )
         collapseKey++
