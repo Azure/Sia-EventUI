dd67ab7c45c57022cb1374eb12d791b41acbb500
Sasha Pierson <sashapie@microsoft.com>
2017-11-10 15:04:56 -0800

    Clarifies what the code examples are and how they are used.

--- cfg/constExample.js
+++ cfg/constExample.js
@@ -1,7 +1,12 @@
+/*  This is example code that goes in the config file(s) you create for your environment(s).  
+You'll probably want to use the code in the 'More than Defaults' section.
+*/
 
 let defaultConstants = require('./defaultConstants')
 
-//Override base constants or add additional constants for this environment
+/* More than Defaults:  Override default constants in cfg/defaultConstants.js file and/or add additional constants for this environment 
+(you'll probably want this if you commit your code to a public repo)
+*/
 module.exports = Object.assign({}, defaultConstants, {
     baseUrl: JSON.stringify("http://localhost:60000/"), //Host on a different port
     retries: 4 //retry failed requests more times before giving up
@@ -9,7 +14,7 @@ module.exports = Object.assign({}, defaultConstants, {
 
 
 
-/* Copy this to a new file to create const file that just uses default constants
+/* Defaults Only:  Copy this to a new file to create const file that just uses default constants
 let defaultConstants = require('./defaultConstants')
 
 module.exports = Object.assign({}, defaultConstants)
