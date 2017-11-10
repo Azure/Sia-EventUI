c431edecaba597a9d83d053e3bfbd6e763f61464
Sasha Pierson <sashapie@microsoft.com>
2017-11-10 15:34:50 -0800

    deleted redundant template file for constants.

--- cfg/exampleConstants.js
+++
@@ -1,8 +0,0 @@
-let baseConstants = require('./defaultConstants')
-
-//Override base constants or add additional constants for this environment
-
-module.exports = Object.assign({}, baseConstants, {
-    baseUrl: JSON.stringify("http://localhost:60000/"), //Host on a different port
-    retries: 4 //retry failed requests more times before giving up
-})
