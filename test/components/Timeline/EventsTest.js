8fb885f3d12113eec01a32fe2ec740c61d8d2d2a
Sasha Pierson <sashapie@microsoft.com>
2017-11-16 12:14:55 -0800

    tests reflect updates to LoadTextFromEvent method

--- test/components/Timeline/EventsTest.js
+++ test/components/Timeline/EventsTest.js
@@ -3,85 +3,126 @@ import { expect } from 'chai'
 import Event from '../../../src/components/Timeline/Event'
 import Events from '../../../src/components/Timeline/Events'
 
-const eventTypeOne = {
-    name: 'One',
+const eventTypeZero = {
+    name: 'Everything is set up',
     displayTemplate: {
-        pattern: 'One is the loneliest number'
+        pattern: 'Valid displayTemplate.pattern'
+    }
+}
+
+const eventTypeOne = {
+    name: 'No displayTemplate.pattern',
+    displayTemplate: {
     }
 }
 
 const eventTypeTwo = {
-    name: 'Two'
+    name: 'Invalid displayTemplate.pattern',
+    displayTemplate: {
+        pattern: ''
+    }
 }
 
 const eventTypeThree = {
-    name: 'Three',
-    displayTemplate: {
-        pattern: ''
-    }
+    name: 'eventType.name'
 }
 
 const eventTypeFour = {
-    name: '',
-    displayTemplate: {
-        pattern: ''
+    
+}
+
+const eventTypeFive = {
+    name: ''
+}
+
+
+const eventZero = {
+    eventTypeId: 0,
+    data: {
+        displayText: 'User displayText',
+        someField: ''
     }
 }
 
+
 const eventOne = {
-    eventTypeId: 0,
+    eventTypeId: 100,
     data: {
-        displayText: 'One, only one',
+        displayText: 'User displayText',
         someField: ''
     }
 }
 
 const eventTwo = {
-    eventTypeId: 1,
+    eventTypeId: 100,
     data: {
-        displayText: '',
+        displayText: 'User displayText',
         someField: ''
     }
 }
 
 const eventThree = {
-    eventTypeId: 2,
+    eventTypeId: 1,
     data: {
-        displayText: 'User display text',
+        displayText: 'User displayText',
         someField: 'User data here'
     }
 }
 
 const eventFour = {
-    eventTypeId: 3,
+    eventTypeId: 2,
     data: {
-        displayText: 'User display text',
+        displayText: 'User displayText',
         someField: 'User data here'
     }
 }
 
 const eventFive = {
-    eventTypeId: 10,
-    data: {
-        displayText: 'User display text',
-        someField: 'User data here'
-    }
+    eventTypeId: 3
 }
 
 const eventSix = {
-    eventTypeId: 50,
+    eventTypeId: 3,
     data: {
         someField: 'User data here'
     }
 }
 
+const eventSeven = {
+    eventTypeId: 3,
+    data: {
+        displayText: '',
+        someField: 'User data here'
+    }
+}
 
-const events = [eventOne, eventTwo, eventThree, eventFour, eventFive, eventSix]
+const eventEight = {
+    eventTypeId: 100,
+    data: {
+        someField: 'User data here'
+    }
+}
+
+const eventNine = {
+    eventTypeId: 100,
+    data: {
+        displayText: '',
+        someField: 'User data here'
+    }
+}
+
+const eventTen = {
+    eventTypeId: 100
+}
+
+const events = [eventZero, eventOne, eventTwo, eventThree, eventFour, 
+    eventFive, eventSix, eventSeven, eventEight, eventNine, eventTen]
 const eventActions = null
 const eventTypeActions = null
 const ticketId = '2'
 const incidentId = null
-const eventTypes = [eventTypeOne, eventTypeTwo, eventTypeThree, eventTypeFour]
+const eventTypes = [eventTypeZero, eventTypeOne, eventTypeTwo, 
+    eventTypeThree, eventTypeFour, eventTypeFive]
 
 
 describe('Events', function test () {
@@ -99,26 +140,47 @@ describe('Events', function test () {
         expect(this.output[0].props.ticketId).to.equal(ticketId)
     })
     it('Should return events with text from the eventType.displayTemplate.pattern', () => {
-        expect(this.output[0].props.text).to.equal('One is the loneliest number')
+        expect(this.output[0].props.text).to.equal('Valid displayTemplate.pattern')
+    })
+
+    it('Should return events with text from the displayText if no eventType', () => {
+        expect(this.output[1].props.text).to.equal('User displayText')
     })
  
-    it('Should return events with text from the eventType.name if no displayTemplate', () => {
-        expect(this.output[1].props.text).to.equal('Two')
+    it('Should return events with text from the displayText if no displayTemplate', () => {
+        expect(this.output[2].props.text).to.equal('User displayText')
     })
 
-    it('Should return events with text from the eventType.name if no displayTemplate.pattern', () => {
-        expect(this.output[2].props.text).to.equal('Three')
+    it('Should return events with text from the displayText if no displayText.pattern', () => {
+        expect(this.output[3].props.text).to.equal('User displayText')
     })
 
-    it('Should return events with load message if eventType has no name or displayTemplate', () => {
-        expect(this.output[3].props.text).to.equal('Loading event information...')
+    it('Should return events with text from the displayText if invalid displayText.pattern', () => {
+        expect(this.output[4].props.text).to.equal('User displayText')
     })
 
-    it('Should return events with text from event displayText when no eventType', () => {
-        expect(this.output[4].props.text).to.equal('User display text')
+    it('Should return events with text from the eventType.name if no eventType.displayTemplate.pattern and no event.data', () => {
+        expect(this.output[5].props.text).to.equal('eventType.name')
     })
 
-    it('Should return events with text from stringified event.data', () => {
-        expect(this.output[5].props.text).to.equal('{"someField":"User data here"}')
+    it('Should return events with text from the eventType.name if no eventType.displayTemplate.pattern and no event.data.displayText', () => {
+        expect(this.output[6].props.text).to.equal('eventType.name')
     })
+
+    it('Should return events with text from the eventType.name if no eventType.displayTemplate.pattern and invalid event.data.displayText', () => {
+        expect(this.output[7].props.text).to.equal('eventType.name')
+    })
+
+    it('Should return events with text from stringified event.data if no eventType, no event.data.displayText, and no event.name', () => {
+        expect(this.output[8].props.text).to.equal('{"someField":"User data here"}')
+    })
+
+    it('Should return events with text from stringified event.data if no eventType, invalid event.data.displayText, and no event.name', () => {
+        expect(this.output[9].props.text).to.equal('{"displayText":"","someField":"User data here"}')
+    })
+
+    it('Should return events with error message when no eventType and no data', () => {
+        expect(this.output[10].props.text).to.equal('This event has no text')
+    })
+
 })
