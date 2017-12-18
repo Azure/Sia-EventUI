f3bcd8dc5a7a2cc835b17f4babc2fa71fb360a9e
Alberto Rodriguez <alrodrig@microsoft.com>
2017-12-18 11:13:23 -0800

    Highlight animation fix

--- src/reducers/eventReducer.js
+++ src/reducers/eventReducer.js
@@ -1,4 +1,5 @@
 import paginated from 'paginated-redux'
+import moment from 'moment'
 import * as eventActions from '../actions/eventActions'
 import { mergeWithOverwrite } from './reducerHelpers'
 
@@ -12,10 +13,10 @@ const makeSearchable = (event) => ({
 const addEventsToState = (state, events) => mergeWithOverwrite(state, events.map(event => makeSearchable(event)))
 
 export const list = (state = defaultEventCollection, action) => {
-    switch(action.type){
+    switch (action.type) {
         case eventActions.RECEIVE_EVENT:
         case eventActions.POST_EVENT_SUCCEED:
-            return addEventsToState(state, [action.event])
+            return addEventsToState(state, [{...action.event, timeReceived: moment()}])
         case eventActions.RECEIVE_EVENTS:
             return addEventsToState(state, action.events)
         default:
