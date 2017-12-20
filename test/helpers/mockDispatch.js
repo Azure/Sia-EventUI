2aa38b33d03e91549d4ab565bf231dcb5cce4a74
Philip Dimitratos <pdimit@microsoft.com>
2017-12-20 13:13:02 -0800

    Cleanup and clarification of mockDispatch

--- test/helpers/mockDispatch.js
+++ test/helpers/mockDispatch.js
@@ -2,6 +2,15 @@ import { isObject } from "util";
 
 
 export const AddMockDispatch = (state) => (mockDispatchRecorder) => {
+    /*
+        Action could be:
+            an object
+            a function that takes dispatch as an argument (thunk middleware action)
+        If the action is a function, call it passing in dispatch.
+        If the action is an object record it; a single action sets the .action property,
+            a second action turns .action into an array.
+            Subsequent actions are added to the array.
+    */
     const dispatch = (action) => isObject(action)
     ? mockDispatchRecorder.action
         ? Array.isArray(mockDispatchRecorder.action)
