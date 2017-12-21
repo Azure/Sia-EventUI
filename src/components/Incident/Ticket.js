915602a4bb9bdefd739217c2486ce8e7bc71b098
Sasha Pierson <RedSquirrelious@users.noreply.github.com>
2017-12-21 15:25:51 -0800

    eventTypeActions.fetchEvents added

--- src/components/Incident/Ticket.js
+++ src/components/Incident/Ticket.js
@@ -8,6 +8,7 @@ import { RetryButton } from '../elements/Buttons'
 import LoadingMessage from '../elements/LoadingMessage'
 import * as incidentActions from '../../actions/incidentActions'
 import * as eventActions from '../../actions/eventActions'
+import * as eventTypeActions from '../../actions/eventTypeActions'
 
 class Ticket extends Component {
     static propTypes = {
@@ -21,8 +22,9 @@ class Ticket extends Component {
     }
 
     componentDidMount() {
-        const { dispatch, incident, ticketId, ticket, ticketSystem, preferences } = this.props
+        const { dispatch, incident, ticketId, ticket, ticketSystem, preferences, history } = this.props
         dispatch(incidentActions.fetchIncidentIfNeeded(incident, ticketId, ticket, ticketSystem, preferences))
+        dispatch(eventTypeActions.fetchEventTypes(history))
     }
 
     render() {
