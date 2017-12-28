e05bd5747c7057a4e1d02e5e06857c09e841720e
Sasha Pierson <RedSquirrelious@users.noreply.github.com>
2017-12-27 18:27:54 -0800

    those methods aren't applicable.  early stab at testing.

--- test/reducers/eventReducerTest.js
+++ test/reducers/eventReducerTest.js
@@ -7,44 +7,44 @@ const defaultState = {}
 
 describe('Event Reducer', function EventReducerTest() {
     describe('filter()', function() {
-        describe('when the provided filter has no type', function () {
-            it('should return the current state', function () {
-                var state = {fozzie: 'bear'}
-                var filterAction = {}
-                expect(filter(state, filterAction)).to.equal(state)
-            })
-        })
-        describe('when the provided filter has a known type', function () {
-            it('should filter to the expected state', function () {
-                var state = { 
-                    fozzie: 'bear', 
-                    miss: 'piggy', 
-                    kermit: 'the frog'
-                }
+        // describe('when the provided filter has no type', function () {
+        //     it('should return the current state', function () {
+        //         var state = {fozzie: 'bear'}
+        //         var filterAction = {}
+        //         expect(filter(state, filterAction)).to.equal(state)
+        //     })
+        // })
+        // describe('when the provided filter has a known type', function () {
+        //     it('should filter to the expected state', function () {
+        //         var state = { 
+        //             fozzie: 'bear', 
+        //             miss: 'piggy', 
+        //             kermit: 'the frog'
+        //         }
 
-                var expectedState = { 
-                    incidentId: 0,
-                    eventTypes: [{id: 1, name: 'TestName'}]                    
-                }
+        //         var expectedState = { 
+        //             incidentId: 0,
+        //             eventTypes: [{id: 1, name: 'TestName'}]                    
+        //         }
 
                 
-                var filterAction = { 
-                    type: 'CHANGE_EVENT_FILTER', 
-                    filter: {
-                        incidentId: 0,
-                        eventTypes: [{id: 1, name: 'TestName'}]
-                    } 
-                }
+        //         var filterAction = { 
+        //             type: 'CHANGE_EVENT_FILTER', 
+        //             filter: {
+        //                 incidentId: 0,
+        //                 eventTypes: [{id: 1, name: 'TestName'}]
+        //             } 
+        //         }
 
-                expect(filter(state, filterAction)).to.eql(expectedState)
-            })
-        })
-        describe('when the provided filter has an unknown type', function () {
-            it('should return the current state', function () {
-                var state = {fozzie: 'bear'}
-                var filterAction = {}
-                expect(filter(state, filterAction)).to.equal(state)
-            })
-        })
+        //         expect(filter(state, filterAction)).to.eql(expectedState)
+        //     })
+        // })
+        // describe('when the provided filter has an unknown type', function () {
+        //     it('should return the current state', function () {
+        //         var state = {fozzie: 'bear'}
+        //         var filterAction = {}
+        //         expect(filter(state, filterAction)).to.equal(state)
+        //     })
+        // })
     })
 })
