a1afdbb8080cc4b1b68e73305f40cf4615b0668c
Sasha Pierson <RedSquirrelious@users.noreply.github.com>
2017-12-27 20:17:58 -0800

    already combining filterReducer with eventReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -5,7 +5,6 @@ import incidents from './incidentReducers'
 import engagements from './engagementReducer'
 import events from './eventReducer'
 import expandSection from './expandSectionReducer'
-import filter from './filterReducer'
 import signalR from './signalRReducer'
 import popup from './popupReducer'
 import forms from './formReducer'
@@ -17,7 +16,6 @@ const rootReducer = (filters) => combineReducers({
     tickets,
     engagements,
     events: events(filters),
-    filter,
     popup,
     forms,
     expandSection,
