587e1c45406eea55d3ed87e71f5e47afc5724270
Philip Dimitratos <pdimit@microsoft.com>
2017-12-29 11:06:52 -0800

    Fixed bugs introduced by reducer restructuring

--- src/components/Timeline/Playbook/DisplayPlaybook.js
+++ src/components/Timeline/Playbook/DisplayPlaybook.js
@@ -37,8 +37,9 @@ export const mapStateToDisplayPlaybookProps = (state, ownProps) => {
     const auth = state.auth
 
     const eventType = state.eventTypes.records[ownProps.eventTypeId]
-    const event = Object.values(state.events.list)
-        .find(event => event.id === ownProps.eventId)
+    const event = state.events.list.list.find(
+        event => event.id === ownProps.eventId
+    )
     const ticket = state.tickets.map[ownProps.ticketId]
     const engagement = state.engagements.list.find(
         engagement => engagement
