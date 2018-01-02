fda3a1dfd13d5bb52dde52c939314b6bb7367f64
Maggie Pint <magpint@microsoft.com>
2018-01-02 10:46:35 -0800

    chrome extension track ticket progress

--- cfg/localhost.js
+++ cfg/localhost.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const webpack = require('webpack')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 
 const baseConfig = require('./base')
 
@@ -15,6 +16,20 @@ config.entry.push(...[
 ])
 
 config.plugins.push(...[
+    new CopyWebpackPlugin([
+      {
+        from: `${defaultSettings.srcPath}/extensionHooks/manifest.json`,
+        transform: function(content, path) {
+          let jsonContent = JSON.parse(content.toString())
+          console.log(jsonContent)
+          console.log(constants.ticketSystems)
+          Object.values(constants.ticketSystems).forEach((system) =>{
+            jsonContent.content_scripts[0].matches.push(`${system.ticketUriPrefix}*`)
+          })
+          return Buffer.from(JSON.stringify(jsonContent))
+        }
+      }
+    ], {copyUnmodified: true}),
   new webpack.HotModuleReplacementPlugin(),
   new webpack.NamedModulesPlugin(),
   new webpack.NoEmitOnErrorsPlugin()
