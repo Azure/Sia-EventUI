687600964a676ec2237d6a762b31f06feb6704ce
Ying Zhao <yizha@microsoft.com>
2018-01-04 14:48:41 -0800

    Disable iconButton when there is no extra information about the event 4.

--- src/components/Timeline/Event.js
+++ src/components/Timeline/Event.js
@@ -13,114 +13,116 @@ import * as eventTypeActions from '../../actions/eventTypeActions'
 
 export const Event = ({
     text,
-    time,
-    backgroundColor,
-    incidentId,
-    ticketId,
-    eventTypeId,
-    eventTypeIsFetching,
-    eventTypeIsError,
-    eventId,
-    event,
-    actions
+  time,
+  backgroundColor,
+  incidentId,
+  ticketId,
+  eventTypeId,
+  eventTypeIsFetching,
+  eventTypeIsError,
+  eventId,
+  event,
+  actions
 }) => {
-    const itemHighlight = (event && event.timeReceived) ? {
-        animationName: 'yellowfade',
-        animationDuration: '30s',
-        animationDelay: -(moment().diff(event.timeReceived, 'seconds')) + 's'
-    } : {}
-    const isAllPlaybookInfoAvailable = actions.length > 0
-    const iconColor = isAllPlaybookInfoAvailable ? 'black':'Lightgrey'
-    
+  const itemHighlight = (event && event.timeReceived) ? {
+    animationName: 'yellowfade',
+    animationDuration: '30s',
+    animationDelay: -(moment().diff(event.timeReceived, 'seconds')) + 's'
+  } : {}
+  const isAllPlaybookInfoAvailable = actions.length > 0
+  const iconColor = isAllPlaybookInfoAvailable ? 'black' : 'Lightgrey'
 
-    return eventTypeIsFetching && !eventHasValidDisplayText(event)
-        ? LoadingMessage('Fetching Event Type Information', eventTypeActions.fetchEventType(eventTypeId))
-        : eventTypeIsError && !eventHasValidDisplayText(event)
-            ? ErrorMessage('Error fetching eventType!', eventTypeActions.fetchEventType(eventTypeId))
-            : <div style={itemHighlight}>
-              
+
+  return eventTypeIsFetching && !eventHasValidDisplayText(event)
+    ? LoadingMessage('Fetching Event Type Information', eventTypeActions.fetchEventType(eventTypeId))
+    : eventTypeIsError && !eventHasValidDisplayText(event)
+      ? ErrorMessage('Error fetching eventType!', eventTypeActions.fetchEventType(eventTypeId))
+      : <div style={itemHighlight}>
+
         <BootstrapPlaybook
-            eventId={eventId}
-            eventTypeId={eventTypeId}
-            ticketId={ticketId}
-            incidentId={incidentId}
+          eventId={eventId}
+          eventTypeId={eventTypeId}
+          ticketId={ticketId}
+          incidentId={incidentId}
         />
-        <Card
-          className="incident-card"
-          style={{ backgroundColor }}
-          expanded={isAllPlaybookInfoAvailable ? null : false}
-        >
-          <CardHeader
-            title={ticketId ? `${ticketId}: ${text}` : text}
-            subtitle={time ? time.format('LTS') : 'Time unknown!'}
-            actAsExpander={true}
-            showExpandableButton={true}
-            iconStyle={{
-              color: iconColor
-            }}
-          />
-          {
-            isAllPlaybookInfoAvailable &&
-            <CardText expandable={true} >
-              Select the Actions below:
+        <div>
+          <Card
+            className="incident-card"
+            style={{ backgroundColor }}
+            expanded={isAllPlaybookInfoAvailable ? null : false}
+          >
+            <CardHeader
+              title={ticketId ? `${ticketId}: ${text}` : text}
+              subtitle={time ? time.format('LTS') : 'Time unknown!'}
+              actAsExpander={true}
+              showExpandableButton={true}
+              iconStyle={{
+                color: iconColor
+              }}
+            />
+            {
+              isAllPlaybookInfoAvailable &&
+              <CardText expandable={true} >
+                Select the Actions below:
                 <Playbook
-                eventId={eventId}
-                eventTypeId={eventTypeId}
-                ticketId={ticketId}
-                incidentId={incidentId}
-                actions={actions}
-              />
-            </CardText>
-          }
-        </Card>
-    </div >
+                  eventId={eventId}
+                  eventTypeId={eventTypeId}
+                  ticketId={ticketId}
+                  incidentId={incidentId}
+                  actions={actions}
+                />
+              </CardText>
+            }
+          </Card>
+        </div>
+      </div >
 }
 
 Event.propTypes = {
-    text: PropTypes.string.isRequired,
-    time: PropTypes.instanceOf(moment),
-    backgroundColor: PropTypes.string,
-    ticketId: PropTypes.string,
-    eventId: PropTypes.number,
-    eventTypeId: PropTypes.number,
-    eventTypeIsFetching: PropTypes.bool,
-    event: PropTypes.object
+  text: PropTypes.string.isRequired,
+  time: PropTypes.instanceOf(moment),
+  backgroundColor: PropTypes.string,
+  ticketId: PropTypes.string,
+  eventId: PropTypes.number,
+  eventTypeId: PropTypes.number,
+  eventTypeIsFetching: PropTypes.bool,
+  event: PropTypes.object
 }
 
 const eventHasValidDisplayText = (event) => event && event.data && event.data.DisplayText
 
 export const mapStateToEventProps = (state, ownProps) => {
-    const event = ownProps.event
-    const eventType = state.eventTypes.records[event.eventTypeId]
-    const ticket = state.tickets.map[ownProps.ticketId]
-    const engagement = state.engagements.list.find(engagement => engagement.id === ownProps.engagementId)
-  
-    const actions = eventType.actions
-    var populatedConditionSetTest = TestConditionSet(event, ticket, eventType, engagement)
-    const qualifiedActions = actions.filter(
-      action => action.conditionSets.reduce(
-        (allConditionSetsMet, currentConditionSet) => allConditionSetsMet
-          ? populatedConditionSetTest(currentConditionSet)
-          : false,
-        true
-      )
+  const event = ownProps.event
+  const eventType = state.eventTypes.records[event.eventTypeId]
+  const ticket = state.tickets.map[ownProps.ticketId]
+  const engagement = state.engagements.list.find(engagement => engagement.id === ownProps.engagementId)
+
+  const actions = eventType.actions
+  var populatedConditionSetTest = TestConditionSet(event, ticket, eventType, engagement)
+  const qualifiedActions = actions.filter(
+    action => action.conditionSets.reduce(
+      (allConditionSetsMet, currentConditionSet) => allConditionSetsMet
+        ? populatedConditionSetTest(currentConditionSet)
+        : false,
+      true
     )
-  
-    return {
-        ...ownProps,
-        ticket,
-        eventType,
-        engagement,
-        eventId: event.id,
-        eventTypeId: event.eventTypeId,
-        eventTypeIsFetching: state.eventTypes.fetching.includes(event.eventTypeId),
-        eventTypeIsError: state.eventTypes.error.includes(event.eventTypeId),
-        time: moment(event.occurred ? event.occurred : event.Occurred),
-        dismissed: event.dismissed,
-        backgroundColor: event.backgroundColor,
-        text: LoadTextFromEvent(event, eventType, ticket, engagement),
-        actions: qualifiedActions
-    }
+  )
+
+  return {
+    ...ownProps,
+    ticket,
+    eventType,
+    engagement,
+    eventId: event.id,
+    eventTypeId: event.eventTypeId,
+    eventTypeIsFetching: state.eventTypes.fetching.includes(event.eventTypeId),
+    eventTypeIsError: state.eventTypes.error.includes(event.eventTypeId),
+    time: moment(event.occurred ? event.occurred : event.Occurred),
+    dismissed: event.dismissed,
+    backgroundColor: event.backgroundColor,
+    text: LoadTextFromEvent(event, eventType, ticket, engagement),
+    actions: qualifiedActions
+  }
 }
 
 export default connect(mapStateToEventProps)(Event)
