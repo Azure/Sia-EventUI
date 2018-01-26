21b0732b17a808305bae3688c540fb9fa81738d4
Sasha Pierson <RedSquirrelious@users.noreply.github.com>
2018-01-26 10:49:13 -0800

    display eventType fetching errors w/formatting

--- src/components/elements/ErrorMessage.js
+++ src/components/elements/ErrorMessage.js
@@ -1,11 +1,20 @@
 import React from 'react'
 import ErrorIcon from 'material-ui/svg-icons/alert/error'
+import { Card, CardHeader } from 'material-ui/Card'
 import { RetryButton } from 'components/elements/Buttons'
 
-const ErrorMessage = (message, actionForRetry) => <div>
-  <ErrorIcon />
-  <span>{message}</span>
-  { actionForRetry ? <RetryButton actionForRetry={actionForRetry} /> : null }
+const ErrorMessage = (message, actionForRetry, time = null, backgroundColor = null, ) => <div>
+  <Card
+    className='incident-card'
+    style={{ backgroundColor }}
+  >
+    <ErrorIcon />
+    <CardHeader
+      title={message}
+      subtitle={time ? time.local().format('LTS') : 'Time unknown!'}
+    />
+    { actionForRetry ? <RetryButton actionForRetry={actionForRetry} /> : null }
+  </Card>
 </div>
 
 export default ErrorMessage
--- test/components/elements/ErrorMessageTest.js
+++ test/components/elements/ErrorMessageTest.js
@@ -1,25 +1,54 @@
 'use strict'
 import { expect } from 'chai'
+import moment from 'moment'
 import createComponent from 'test/helpers/shallowRenderHelper'
 import ErrorMessage from 'components/elements/ErrorMessage'
+import { Card, CardHeader } from 'material-ui/Card'
 import ErrorIcon from 'material-ui/svg-icons/alert/error'
 import { RetryButton } from 'components/elements/Buttons'
 
-describe('ErrorMessage', function () {
-  it('Should render an ErrorIcon icon and a span with the given message when called with just a message', function () {
+describe.only('ErrorMessage', function () {
+  describe('when given only a message', function() {
     const testObject = ErrorMessage('TestMessage')
 
-    expect(testObject.type).to.equal('div')
-    expect(testObject.props.children[0].type).to.equal(ErrorIcon)
-    expect(testObject.props.children[1].type).to.equal('span')
-    expect(testObject.props.children[1].props.children).to.equal('TestMessage')
-    expect(testObject.props.children[2]).to.be.null
+    it('Should render a Card with an ErrorIcon icon and a CardHeader with the given message', function () {
+      expect(testObject.type).to.equal('div')
+      expect(testObject.props.children.type).to.equal(Card)
+      expect(testObject.props.children.props.children[0].type).to.equal(ErrorIcon)
+      expect(testObject.props.children.props.children[1].type).to.equal(CardHeader)
+      expect(testObject.props.children.props.children[1].props.title).to.equal('TestMessage')
+    })
+
+    it("should render a card with a CardHeader with the subtitle 'Time unknown!", function () {
+      expect(testObject.props.children.props.children[1].props.subtitle).to.equal('Time unknown!')
+    })
   })
 
-  it('Should additionally render a retry button when provided an action for retry', function () {
+  describe('when given a message and provided an action for retry', function() {
     const testObject = ErrorMessage('TestMessage', 'TestAction')
 
-    expect(testObject.props.children[2].type).to.equal(RetryButton)
-    expect(testObject.props.children[2].props.actionForRetry).to.equal('TestAction')
+    it('Should additionally render a retry button', function () {
+      expect(testObject.props.children.props.children[2].type).to.equal(RetryButton)
+      expect(testObject.props.children.props.children[2].props.actionForRetry).to.equal('TestAction')
+    })
+  })
+
+  describe('when given a message, an action, and a time', function () {
+    const backgroundColor = 'Purple'
+    const testTime = moment()
+    const testObject = ErrorMessage('TestMessage', 'TestAction', testTime)
+
+    it('should render a card with a CardHeader with the given time as a subtitle', function () {
+      expect(testObject.props.children.props.children[1].props.subtitle).to.equal(testTime.local().format('LTS'))
+    })
+  })
+
+  describe('when given a message, an action, and a background color', function () {
+    const backgroundColor = 'Purple'
+    const testObject = ErrorMessage('TestMessage', 'TestAction', null, backgroundColor)
+
+    it('should render a card styled with that background color', function () {
+      expect(testObject.props.children.props.style).to.deep.equal({ backgroundColor: 'Purple' })
+    })
   })
 })
