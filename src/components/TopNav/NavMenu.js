6458c53c74eae2fbbd809828f4d1a9d215b03f96
Strand McCutchen <strand@bettermistak.es>
2018-02-08 11:27:42 -0800

    Provide breadcrumbs of prev visited tickets (#75)
    * Provide breadcrumbs of prev visited tickets
    
    * Revise based on feedback
    
    * add specs verifying link generation
    
    * improve test coverage in NavMenu#mapStateToProps

--- src/components/TopNav/NavMenu.js
+++ src/components/TopNav/NavMenu.js
@@ -1,27 +1,44 @@
 import React from 'react'
+import { Link, withRouter } from 'react-router-dom'
 import PropTypes from 'prop-types'
-import {connect} from 'react-redux'
+import { connect } from 'react-redux'
 import MenuItem from 'material-ui/MenuItem'
 import IconMenu from 'material-ui/IconMenu'
-import { Link } from 'react-router-dom'
 import NavigationMenu from 'material-ui/svg-icons/navigation/menu'
 import IconButton from 'material-ui/IconButton'
 import * as auth from 'services/authNService'
 
-export const NavMenu = ({ dispatch }) =>
-  <IconMenu
+var transformIdToTicketLink = (id, index) =>
+  <MenuItem
+    key={'ticket-' + id + '-index-' + index}
+    primaryText={<Link to={'/tickets/' + id} >{'Ticket ' + id}</Link>}
+  />
 
+export const NavMenu = ({ dispatch, history, ticketIds }) => {
+  return (<IconMenu
     iconButtonElement={<IconButton><NavigationMenu /></IconButton>}
     anchorOrigin={{horizontal: 'left', vertical: 'bottom'}}
     targetOrigin={{horizontal: 'left', vertical: 'top'}}
->
-
-    <MenuItem primaryText={<Link to='/search' >Incident Search</Link>} />
-    <MenuItem primaryText={<Link to='/' onClick={() => dispatch(auth.logOut)}>LogOut</Link>} />
-    <MenuItem primaryText={<Link to='/debug' >Debug</Link>} />
-  </IconMenu>
+  >
+    <MenuItem key='search' primaryText={<Link to='/search' >Incident Search</Link>} />
+    <MenuItem key='logout' primaryText={<Link to='/' onClick={() => dispatch(auth.logOut)}>LogOut</Link>} />
+    { ticketIds && ticketIds.map(transformIdToTicketLink) }
+    <MenuItem key='debug' primaryText={<Link to='/debug' >Debug</Link>} />
+  </IconMenu>)
+}
 
 NavMenu.propTypes = {
-  dispatch: PropTypes.func
+  dispatch: PropTypes.func,
+  ticketIds: PropTypes.array
 }
-export default connect()(NavMenu)
+
+export const mapStateToProps = (state, ownProps) => {
+  var pathname = ownProps.location.pathname
+  var currentId = /\d/.test(pathname) && pathname.match(/(\d+)/)[1]
+  return {
+    ...ownProps,
+    ticketIds: Object.keys(state.tickets.map).filter(id => id !== currentId)
+  }
+}
+
+export default withRouter(connect(mapStateToProps)(NavMenu))
