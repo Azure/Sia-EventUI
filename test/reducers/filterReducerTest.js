6d73f485e50eddd030b4e1b0e78b24c5fe0f1d66
Philip Dimitratos <pdimit@microsoft.com>
2018-03-12 12:38:16 -0700

    added test case for initialized filter state

--- test/reducers/filterReducerTest.js
+++ test/reducers/filterReducerTest.js
@@ -32,25 +32,33 @@ const actions = {
 const changedEventFilter = { incidentId: 1, ticketId: 0, eventTypes: [1, 2, 3] }
 
 describe('filterReducer', function () {
-  it('should return the initial state with no filter in URL', function () {
-    expect(filterReducer(defaultFilterNoFilterInUrl)(undefined, {})).to.equal(null)
-  })
-  it('should return the initial state with filter in URL', function () {
-    expect(filterReducer(defaultFilterSomethingInUrl)(undefined, {})).to.deep.equal({eventTypes: [1]})
-  })
-  it('should return the initial state with filter in URL when passed action other than CHANGE_EVENT_TYPES', function () {
-    expect(filterReducer(defaultFilterSomethingInUrl)(undefined, actions.noChange)).to.deep.equal({ eventTypes: [1] })
-  })
-  it('should return a new filter when passed a good filter that is different than current filter', function () {
-    expect(filterReducer(defaultFilterNoFilterInUrl)(undefined, actions.change)).to.deep.equal(newFilterWithIncidentId)
-  })
-  it('should return a new filter with default null ticketId when passed a filter w/no ticketId', function () {
-    expect(filterReducer(defaultFilterNoFilterInUrl)(undefined, actions.badChangeOne)).to.deep.equal({incidentId: 0, ticketId: null, eventTypes: [1]})
-  })
-  it('should return a new filter with default empty eventTypes when passed a filter w/no eventTypes', function () {
-    expect(filterReducer(defaultFilterNoFilterInUrl)(undefined, actions.badChangeTwo)).to.deep.equal({incidentId: 0, ticketId: 0, eventTypes: []})
-  })
-  it('should return a new filter with incidentId cleared when passed action as CLEAR_EVENT_FILTER_INCIDENTID', function(){
-    expect(filterReducer(defaultFilterSomethingInUrl)(undefined, actions.clearIncidentId)).to.deep.equal({ticketId: 0, eventTypes: [1, 2, 3]})
+  describe('from default state', function () {
+    it('should return the initial state with no filter in URL', function () {
+      expect(filterReducer(defaultFilterNoFilterInUrl)(undefined, {})).to.equal(null)
+    })
+    it('should return the initial state with filter in URL', function () {
+      expect(filterReducer(defaultFilterSomethingInUrl)(undefined, {})).to.deep.equal({eventTypes: [1]})
+    })
+    it('should return the initial state with filter in URL when passed action other than CHANGE_EVENT_TYPES', function () {
+      expect(filterReducer(defaultFilterSomethingInUrl)(undefined, actions.noChange)).to.deep.equal({ eventTypes: [1] })
+    })
+    it('should return a new filter when passed a good filter that is different than current filter', function () {
+      expect(filterReducer(defaultFilterNoFilterInUrl)(undefined, actions.change)).to.deep.equal(newFilterWithIncidentId)
+    })
+    it('should return a new filter with default null ticketId when passed a filter w/no ticketId', function () {
+      expect(filterReducer(defaultFilterNoFilterInUrl)(undefined, actions.badChangeOne)).to.deep.equal({incidentId: 0, ticketId: null, eventTypes: [1]})
+    })
+    it('should return a new filter with default empty eventTypes when passed a filter w/no eventTypes', function () {
+      expect(filterReducer(defaultFilterNoFilterInUrl)(undefined, actions.badChangeTwo)).to.deep.equal({incidentId: 0, ticketId: 0, eventTypes: []})
+    })
+    it('should return a new filter with incidentId cleared when passed action as CLEAR_EVENT_FILTER_INCIDENTID', function(){
+      expect(filterReducer(defaultFilterSomethingInUrl)(undefined, actions.clearIncidentId)).to.deep.equal({ticketId: 0, eventTypes: [1, 2, 3]})
+    })
+  }),
+
+  describe('from initialized state', function () {
+    it('should return a new filter with incidentId cleared when passed action as CLEAR_EVENT_FILTER_INCIDENTID', function(){
+      expect(filterReducer(defaultFilterSomethingInUrl)(changedEventFilter, actions.clearIncidentId)).to.deep.equal({ticketId: 0, eventTypes: [1, 2, 3]})
+    })
   })
 })
