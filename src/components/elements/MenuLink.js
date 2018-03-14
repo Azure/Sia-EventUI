ef4ecceda9e447ab3b1d115ba47024661311464c
Sasha Pierson <RedSquirrelious@users.noreply.github.com>
2018-03-14 15:03:12 -0700

    removed brittle title capitalization and related tests

--- src/components/elements/MenuLink.js
+++ src/components/elements/MenuLink.js
@@ -5,20 +5,14 @@ import MenuItem from 'material-ui/MenuItem'
 import ActionDelete from 'material-ui/svg-icons/action/delete'
 
 export const MenuLink = (type, id, onClick, dispatch) => {
-  validateType(type)
-  var typeTitle = type.charAt(0).toUpperCase() + type.slice(1)
   var typeRoute = `/${type}s/${id}`
   return (
     <MenuItem
       key={`${type}-${id}`}
-      primaryText={<Link to={typeRoute}>{`${typeTitle} ${id}`}</Link>}
+      primaryText={<Link to={typeRoute}>{`${type} ${id}`}</Link>}
       rightIcon={<ActionDelete onClick={() => dispatch(onClick(id))} />}
     />
   )
-}
-
-const validateType = (type) => {
-  if (type === undefined || type === null || type.length < 2) { throw new Error('the type input is invalid') }
 }
 
 MenuLink.propTypes = {
--- test/components/elements/MenuLinkTest.js
+++ test/components/elements/MenuLinkTest.js
@@ -29,8 +29,7 @@ describe('MenuLink', function() {
     })
 
     it('should have a route named for the type and id', function () {
-      const mockLinkText = mockType.charAt(0).toUpperCase() + mockType.slice(1)
-      expect(result.props.primaryText.props.children).to.equal(`${mockLinkText} ${mockId}`)
+      expect(result.props.primaryText.props.children).to.equal(`${mockType} ${mockId}`)
     })
 
     it('should have a key that is composed of both the type and id', function() {
@@ -43,33 +42,6 @@ describe('MenuLink', function() {
 
     it('should contain a rightIcon that sets off a function', function() {
       expect(typeof(result.props.rightIcon.props.onClick)).to.equal('function')
-    })
-  })
-
-  describe('when type input is not valid', function() {
-    const mockId = 0
-    const mockOnClick = () => 'clicked'
-    const mockDispatch = (input) => input
-
-    describe('when type is null', function() {
-      const mockType = null
-      it('should throw an error', function() {
-        expect(() => MenuLink(mockType, mockId, mockOnClick, mockDispatch)).to.throw('the type input is invalid')
-      })
-    })
-
-    describe('when type is undefined', function () {
-      const mockType = undefined
-      it('should throw an error', function () {
-        expect(() => MenuLink(mockType, mockId, mockOnClick, mockDispatch)).to.throw('the type input is invalid')
-      })
-    })
-
-    describe('when type input is less than 2 characters', function () {
-      const mockType = ''
-      it('should throw an error', function () {
-        expect(() => MenuLink(mockType, mockId, mockOnClick, mockDispatch)).to.throw('the type input is invalid')
-      })
     })
   })
 })
