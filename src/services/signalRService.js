350d01dd150e2eb177423297e2d75d0c56542b17
Philip Dimitratos <pdimit@microsoft.com>
2018-03-15 15:29:47 -0700

    Updated to latest version of signalR client, reconcicled incompatibilities

--- src/services/signalRService.js
+++ src/services/signalRService.js
@@ -1,4 +1,4 @@
-import * as signalR from '@aspnet/signalr-client'
+import * as signalR from '@aspnet/signalr'
 
 import { getEventActionSet } from 'actions/eventActions'
 import * as signalRActions from 'actions/signalRActions'
@@ -11,23 +11,29 @@ const defaultScopes = [config.clientId]
 
 let signalRConnectionSingleton
 
-export const getSignalRConnection = (dispatch) => signalRConnectionSingleton || resetSignalRConnection(dispatch)
+export const getSignalRConnection = (dispatch) => signalRConnectionSingleton
+  ? Promise.resolve(signalRConnectionSingleton)
+  : resetSignalRConnection(dispatch)
 
-export const resetSignalRConnection = (dispatch) => signalReduxConnection(dispatch)
+export const resetSignalRConnection = (dispatch) => {
+  return signalReduxConnection(dispatch)
     .then(connection => {
       signalRConnectionSingleton = connection
-      return connection
+      return signalRConnectionSingleton
     })
+}
 
 const signalReduxConnection = (dispatch) => configureConnection(dispatch)
-  .then(connection => {
-    startConnection(connection, dispatch)
-    return connection
-  })
+  .then(connection => startConnection(connection, dispatch))
 
 const configureConnection = (dispatch) => getToken(defaultScopes)
   .then(token => {
-    let connection = new signalR.HubConnection(defaultBasePath + 'events/live?token=' + token)
+    const hubAndHttpOptions = {
+      accessTokenFactory: () => token,
+      transport: signalR.TransportType.WebSockets
+    }
+
+    let connection = new signalR.HubConnection(defaultBasePath + 'events/live', hubAndHttpOptions)
 
     connection.on('Send', (event) => {
       const eventObject = JSON.parse(event)
@@ -38,14 +44,22 @@ const configureConnection = (dispatch) => getToken(defaultScopes)
     connection.onClosed = (error) => dispatch(signalRActions.connectionClosed(error ? error.message : 'No Error Message', error ? error.stack : 'No Stack Trace'))
 
     return connection
-  }
-)
+  })
+
 
 const startConnection = (connection, dispatch) => {
   dispatch(signalRActions.tryEstablishConnection())
-  connection.start()
-        .then(() => dispatch(signalRActions.succeedEstablishConnection()),
-            (error) => dispatch(signalRActions.failEstablishConnection(error ? error.message : 'No error message', error ? error.stack : 'No stack trace provided')))
+  return connection
+    .start()
+    .then(() => {
+        dispatch(signalRActions.succeedEstablishConnection())
+        return connection
+      },
+      (error) => {
+        dispatch(signalRActions.failEstablishConnection(error ? error.message : 'No error message', error ? error.stack : 'No stack trace provided'))
+        return connection
+      }
+    )
 }
 
 export const updateEventFilter = (eventFilter) => signalRConnectionSingleton.invoke('updateFilter', eventFilter)
