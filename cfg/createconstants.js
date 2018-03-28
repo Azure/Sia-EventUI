3b4a46f68c2ddba097a43ad35ea49793f2386964
Sasha Pierson <RedSquirrelious@users.noreply.github.com>
2018-03-28 14:57:40 -0700

    use fewer dist env variables

--- cfg/createconstants.js
+++ cfg/createconstants.js
@@ -7,17 +7,6 @@ let distConstants = Object.assign({}, baseConstants, {
   aadInstance: process.env.aadInstance,
   aadTenant: process.env.aadTenant,
   clientId: process.env.clientId,
-  baseUrl: process.env.baseUrl,
-  authRedirectUri: process.env.authRedirectUri,
-  authVersion: process.env.authVersion, // TODO: Add env var
-  ticketSystems: {
-    1: {
-      id: 1,
-      name: 'Default Ticket System',
-      ticketUriPrefix: process.env.ticketUriPrefix,
-      ticketUriSuffix: process.env.ticketUriSuffix,
-    }
-  }
 })
 
 module.exports = distConstants
