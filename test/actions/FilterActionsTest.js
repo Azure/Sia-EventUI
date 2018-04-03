8941225ff26e6193bfa482551f693f2035ba88f6
Alberto Rodriguez <alrodrig@microsoft.com>
2018-04-03 16:36:12 -0700

    Bug fix: addFilter fails for event type Note, which has falsy id: 0.

--- test/actions/FilterActionsTest.js
+++ test/actions/FilterActionsTest.js
@@ -5,7 +5,16 @@ import * as filterActions from 'actions/filterActions'
 
 // Istanbul for test coverage, provides us with a new command nyc-mocha, which runs the tests and gives a reporter
 describe('FilterActions', function () {
-
+  describe('addFilter', function () {
+    it('should return a function for eventType with falsy id: 0', function () {
+      const history = {}
+      const filter = {}
+      const signalRFilterType = {}
+      const eventType = { id: 0, name: 'Note' }
+      const result = filterActions.addFilter(history)(filter, signalRFilterType)(eventType)
+      expect(result).to.be.a('function')
+    })
+  })
 
   describe('changeEventFilter', function () {
     const history = []
