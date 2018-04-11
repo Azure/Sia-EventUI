0cfdd9ba383ce69eda94d2a21b589e241e01e79a
Daniel Jurek <daniel.jurek@microsoft.com>
2018-04-11 10:48:15 -0700

    Show truncated ticket titles in Nave Menu history

--- src/components/elements/MenuLink.js
+++ src/components/elements/MenuLink.js
@@ -2,17 +2,39 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
 import MenuItem from 'material-ui/MenuItem'
+import ActionDelete from 'material-ui/svg-icons/action/delete'
+import ActionLabel from 'material-ui/svg-icons/action/label'
 
-export const MenuLink = (
-  primaryText,
-  route,
-  rightIcon
-) => <MenuItem
-  key={primaryText + route}
-  containerElement={<Link to={route} />}
-  primaryText={primaryText}
-  rightIcon={rightIcon}
-/>
+const styles = {
+  constrainedWidth: {
+    overflow: 'hidden',
+    textOverflow: 'ellipsis',
+    width: '25rem'
+  }
+}
+
+export const MenuLink = ({type, id, title, onClick, dispatch}) => {
+  let typeRoute = `/${type}s/${id}`
+  let menuItemKey = `${type}-${id}`
+
+  return (
+    <div title={title}>
+      <MenuItem
+        key={menuItemKey}
+        containerElement={<Link to={typeRoute} />}
+        primaryText={ticketPrimaryText(id, title)}
+        leftIcon={<ActionLabel />}
+        rightIcon={<ActionDelete onClick={() => dispatch(onClick(id))} />}
+      />
+    </div>
+  )
+}
+
+const ticketPrimaryText = (id, title) => {
+  const ticketText = `${id} ${title ? '-' : ''} ${title}`
+
+  return (<div style={styles.constrainedWidth}>{ticketText}</div>)
+}
 
 MenuLink.propTypes = {
   primaryText: PropTypes.string.isRequired,
